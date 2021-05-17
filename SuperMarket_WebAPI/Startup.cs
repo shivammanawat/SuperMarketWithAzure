3eb6f938280a19be37c0cce4e70068305035b5d2
802281 <802281@LTIN276458>
2021-05-17 18:49:01 +0530

    Updated config

--- SuperMarket_WebAPI/Startup.cs
+++ SuperMarket_WebAPI/Startup.cs
@@ -1,15 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SuperMarket.Client;
+using SuperMarket.Logging;
+using SuperMarket.Repository;
+using SuperMarket.Service;
+using Supermarket_WebAPI.Service;
+using Microsoft.Azure.ServiceBus;
+using Azure.Storage.Blobs;
 
 namespace SuperMarket_WebAPI
 {
@@ -25,7 +25,32 @@ namespace SuperMarket_WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<PaymentGatewayClient>();
+
+            services.AddSingleton<IStockRepository, StockRepository>();
+            services.AddSingleton<IStockService, StockService>();
+            services.AddHttpClient<IPaymentGatewayClient, PaymentGatewayClient>();
+            services.AddSingleton<IDiscountRepository, DiscountRepository>();
+            services.AddSingleton<IDiscountService, DiscountService>();
+
+            services.AddSingleton<ILogger, Logger>();
+
+
+            //services.AddSingleton<ITopicClient>(serviceProvider => new TopicClient(connectionString: Configuration.GetValue<string>("servicebus:connectionstring"), entityPath: Configuration.GetValue<string>("serviceBus:topicname")));
+            //services.AddSingleton<IMessagePublisher, MessagePublisher>();
+
+            //services.AddScoped<IFileManager, FileManager>();
+
+            //services.AddScoped(_ =>
+            //{
+            //    return new BlobServiceClient(Configuration.GetSection("servicebus").GetSection("AzureBlobStorage").Value);
+            //});
+
             services.AddControllers();
+
+
+            services.AddSwaggerGen();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +66,15 @@ namespace SuperMarket_WebAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;
+            });
 
             app.UseEndpoints(endpoints =>
             {
